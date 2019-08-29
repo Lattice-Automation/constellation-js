7d575f3a49817de2899b3ba7c4bada81eddf0d19
leshane <leshanek@gmail.com>
2019-08-29 11:14:34 -0400

    temp commit implementing serverless

--- demos/server.js
+++ demos/server.js
@@ -1,55 +1,68 @@
-let express = require('express');
-let app = express();
-let path = require('path');
-let bodyParser = require('body-parser');
-let FormData = require('form-data');
-const Readable = require('stream').Readable;
-const xmlparser = require('express-xml-bodyparser');
+const express = require("express");
+var sls = require("serverless-http");
+const app = express();
+let path = require("path");
+let bodyParser = require("body-parser");
+let FormData = require("form-data");
+const Readable = require("stream").Readable;
+const xmlparser = require("express-xml-bodyparser");
 
-let constellation = require('../lib/constellation');
+let constellation = require("../lib/constellation");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set('json spaces', 1);
+app.set("json spaces", 1);
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.use(express.static(__dirname + '/static'));
-// app.use(express.static(__dirname + '/static/css'));
-// app.use(express.static(__dirname + '/static/js'));
-app.use(express.static(__dirname + '/../lib/'));
+app.use(express.static(__dirname + "/static"));
+app.use(express.static(__dirname + "/../lib/"));
 
-const server = app.listen(8082, function () {
-  console.log('Listening on port %d', server.address().port);
-  console.log('http://localhost:8082/');
+// const server = app.listen(8082, function() {
+//   console.log("Listening on port %d", server.address().port);
+//   console.log("http://localhost:8082/");
+// });
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/static/client.html"));
 });
 
-app.get('/', function(req,res) {
-  res.sendFile((path.join(__dirname + '/static/client.html')));
+app.get("/hello", async (req, res, next) => {
+  res.status(200).send("Hello World!");
 });
 
-app.post('/postSpecs', function(req,res) {
+app.post("/postSpecs", function(req, res) {
   let designName = req.body.designName;
   let langText = req.body.specification;
   let categories = req.body.categories;
   let numDesigns = req.body.numDesigns;
   let maxCycles = req.body.maxCycles;
   let representation = req.body.representation;
-  console.log('---Received new input---');
-  console.log('Design Name: ', designName);
-  console.log('Specification: ', langText);
-  console.log('Categories: ', categories);
-  console.log('numDesigns: ', numDesigns);
-  console.log('maxCycles: ', maxCycles);
-  console.log('Representation:', representation)
+  console.log("---Received new input---");
+  console.log("Design Name: ", designName);
+  console.log("Specification: ", langText);
+  console.log("Categories: ", categories);
+  console.log("numDesigns: ", numDesigns);
+  console.log("maxCycles: ", maxCycles);
+  console.log("Representation:", representation);
 
   let data;
   try {
-    data = constellation.goldbar(designName, langText, categories, numDesigns, maxCycles, representation);
+    data = constellation.goldbar(
+      designName,
+      langText,
+      categories,
+      numDesigns,
+      maxCycles,
+      representation
+    );
     res.status(200).send(data);
   } catch (error) {
     console.log(error);
@@ -57,7 +70,7 @@ app.post('/postSpecs', function(req,res) {
   }
 });
 
-app.post('/importSBOL', xmlparser(), async function(req,res) {
+app.post("/importSBOL", xmlparser(), async function(req, res) {
   let data;
   try {
     data = await constellation.sbol(req.rawBody);
@@ -68,29 +81,33 @@ app.post('/importSBOL', xmlparser(), async function(req,res) {
   }
 });
 
-app.post('/sendToKnox', function(req,res) {
-
+app.post("/sendToKnox", function(req, res) {
   let form = new FormData();
-  let sbolDocs = JSON.parse(req.body['sbolDocs[]']);
+  let sbolDocs = JSON.parse(req.body["sbolDocs[]"]);
 
-  for(let sbol of sbolDocs){
+  for (let sbol of sbolDocs) {
     let stream = new Readable();
     stream.push(sbol);
     stream.push(null);
-    form.append('inputSBOLFiles[]', stream, {
-      filename : 'test.xml',
-      contentType: 'application/xml',
+    form.append("inputSBOLFiles[]", stream, {
+      filename: "test.xml",
+      contentType: "application/xml",
       knownLength: sbol.length
     }); //extra fields necessary
   }
-  form.append('outputSpaceID', req.body.designName);
+  form.append("outputSpaceID", req.body.designName);
 
-  form.submit('http://localhost:8080/sbol/importCombinatorial', function(error, result) {
+  form.submit("http://localhost:8080/sbol/importCombinatorial", function(
+    error,
+    result
+  ) {
     if (error) {
-      console.log('Error!');
+      console.log("Error!");
       res.status(405).send(String(error));
-    } else{
+    } else {
       res.status(200).send(String(result));
     }
   });
 });
+
+module.exports.handler = sls(app);
---
+++ server.js
@@ -0,0 +1,113 @@
+const express = require("express");
+var sls = require("serverless-http");
+const app = express();
+let path = require("path");
+let bodyParser = require("body-parser");
+let FormData = require("form-data");
+const Readable = require("stream").Readable;
+const xmlparser = require("express-xml-bodyparser");
+
+let constellation = require("./lib/constellation");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("json spaces", 1);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(express.static(__dirname + "/demos/static"));
+app.use(express.static(__dirname + "/lib/"));
+
+// const server = app.listen(8082, function() {
+//   console.log("Listening on port %d", server.address().port);
+//   console.log("http://localhost:8082/");
+// });
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/demos/static/client.html"));
+});
+
+app.get("/hello", async (req, res, next) => {
+  res.status(200).send("Hello World!");
+});
+
+app.post("/postSpecs", function(req, res) {
+  let designName = req.body.designName;
+  let langText = req.body.specification;
+  let categories = req.body.categories;
+  let numDesigns = req.body.numDesigns;
+  let maxCycles = req.body.maxCycles;
+  let representation = req.body.representation;
+  console.log("---Received new input---");
+  console.log("Design Name: ", designName);
+  console.log("Specification: ", langText);
+  console.log("Categories: ", categories);
+  console.log("numDesigns: ", numDesigns);
+  console.log("maxCycles: ", maxCycles);
+  console.log("Representation:", representation);
+
+  let data;
+  try {
+    data = constellation.goldbar(
+      designName,
+      langText,
+      categories,
+      numDesigns,
+      maxCycles,
+      representation
+    );
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(405).send(String(error));
+  }
+});
+
+app.post("/importSBOL", xmlparser(), async function(req, res) {
+  let data;
+  try {
+    data = await constellation.sbol(req.rawBody);
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(String(error));
+  }
+});
+
+app.post("/sendToKnox", function(req, res) {
+  let form = new FormData();
+  let sbolDocs = JSON.parse(req.body["sbolDocs[]"]);
+
+  for (let sbol of sbolDocs) {
+    let stream = new Readable();
+    stream.push(sbol);
+    stream.push(null);
+    form.append("inputSBOLFiles[]", stream, {
+      filename: "test.xml",
+      contentType: "application/xml",
+      knownLength: sbol.length
+    }); //extra fields necessary
+  }
+  form.append("outputSpaceID", req.body.designName);
+
+  form.submit("http://localhost:8080/sbol/importCombinatorial", function(
+    error,
+    result
+  ) {
+    if (error) {
+      console.log("Error!");
+      res.status(405).send(String(error));
+    } else {
+      res.status(200).send(String(result));
+    }
+  });
+});
+
+module.exports.handler = sls(app);
